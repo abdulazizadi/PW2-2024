6f114d187514377228a28b9cccf2c576a57cae86
abdulazizadi <146091146+abdulazizadi@users.noreply.github.com>
2024-04-02 15:53:44 +0700

    Add files via upload

---
+++ P6-PW2/dbkoneksi.php
@@ -0,0 +1,11 @@
+<?php
+// Konfigurasi database
+$host = 'localhost'; 
+$dbname = 'dbpuskesmas'; 
+$user = 'root'; 
+$pass = ''; 
+
+$dsn = "mysql:host=$host;dbname=$dbname";
+
+// Buat koneksi Database
+$dbh = new PDO($dsn, $user, $pass);
